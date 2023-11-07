0db1339dc3195ee875ab2c7895954256af052b38
GOPINATH <GOPINATH@LAPTOP-E475Q6GJ.mshome.net>
2023-11-07 21:07:14 +0530

    First commit....

---
+++ src/Test/Test1.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Test1 {
+	public static void main(String args[]) {
+		String hungry="yes";
+		if (hungry=="yes")
+		{
+			System.out.println("need some food");
+		}
+		else
+		{
+			System.out.println("do work....");
+		}
+	}
+}
